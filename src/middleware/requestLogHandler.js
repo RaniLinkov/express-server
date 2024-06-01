9b691214646e31530dc6cc7bfc1e54e39bbe193e
Rani <lrani@netapp.com>
2024-06-01 14:34:55 +0300

    Reversed condition
    Signed-off-by: Rani <lrani@netapp.com>

--- src/middleware/requestLogHandler.js
+++ src/middleware/requestLogHandler.js
@@ -9,15 +9,13 @@ const handler = (req, res, next) => {
         req.logger = req.logger.child(options);
     };
 
-    if (method === "OPTIONS") {
-        return next();
+    if (method !== "OPTIONS") {
+        req.logger.info(`Start: ${method} to ${originalUrl}`);
+
+        res.on("finish", () => {
+            req.logger.info(`End: ${method} to ${originalUrl} with status ${res.statusCode}`);
+        });
     }
-
-    req.logger.info(`Start: ${method} to ${originalUrl}`);
-
-    res.on("finish", () => {
-        req.logger.info(`End: ${method} to ${originalUrl} with status ${res.statusCode}`);
-    });
 
     next();
 };
